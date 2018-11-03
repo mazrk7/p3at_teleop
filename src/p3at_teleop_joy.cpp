ee0304a55b6c685a30150ef0459beb59c4bb3bd3
Vinicius Schettino <v.schettino17@imperial.ac.uk>
2018-11-03 15:58:10 +0000

    First commit of P3AT stuff

--- src/p3at_teleop_joy.cpp
+++ src/p3at_teleop_joy.cpp
@@ -1,15 +1,19 @@
 #include <ros/ros.h>
+// Necessary include files for message types
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
+// Class to teleoperate the P3AT (& other P2OS robots)
 class TeleopP3AT 
 {
     public:
-        TeleopP3AT(ros::NodeHandle &nh, ros::NodeHandle &nh_priv);
-
-    private:
+        // Constructor that takes the public nodehandle to subscribe to and advertise topics
+        // As well as other properties related to joystick management
+        TeleopP3AT(ros::NodeHandle &nh, int lin, int ang, double ang_s, double lin_s);
+        // Callback function for our joystick commands
         void joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg);
         
+    private:   
         // Axes of the joystick
         int linear_, angular_;
         double lin_scale_, ang_scale_;
@@ -22,17 +26,15 @@ class TeleopP3AT
 
 
 // Default constructor for our teleoperator class
-TeleopP3AT::TeleopP3AT(ros::NodeHandle &nh, ros::NodeHandle &nh_priv) 
+TeleopP3AT::TeleopP3AT(ros::NodeHandle &nh, int lin, int ang, double ang_s, double lin_s) 
+                : linear_(lin),
+                  angular_(ang),
+                  ang_scale_(ang_s),
+                  lin_scale_(lin_s)
 {
-    // Set the parameters and take default values if none provided
-    nh_priv.param<int>("axis_linear", linear_, 1);
-    nh_priv.param<int>("axis_angular", angular_, 2);
-    nh_priv.param<double>("scale_angular", ang_scale_, 0.5);
-    nh_priv.param<double>("scale_linear", lin_scale_, 0.5);
-
     // Publisher and subscriber buffer to a queue size of 10 messages 
     vel_pub_ = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
-    // Subscriber 3rd and 4th arguments are the class method and instance pointed to for the callback
+    // Subscriber 3rd and 4th arguments are the callback member function and instance pointed to for the callback
     joy_sub_ = nh.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopP3AT::joyCallback, this);
 }
 
@@ -53,12 +55,23 @@ int main(int argc, char** argv)
 {
     // Initialise our ROS node
     ros::init(argc, argv, "teleop_p3at");
-    // Public & private node handles
+    // Public node handle for global namespaces
     ros::NodeHandle nh;
+    // Private handle for the current node namespace
+    // Access parameters found in /<node_name>/<param_name> e.g. /teleop_p3at/axis_linear 
     ros::NodeHandle nh_priv("~");
     
+    // Read parameters from the parameter server (check where they're set in the launch file)
+    // Take default values if none are provided
+    int linear, angular;
+    double ang_scale, lin_scale;
+    nh_priv.param<int>("axis_linear", linear, 1);
+    nh_priv.param<int>("axis_angular", angular, 2);
+    nh_priv.param<double>("scale_angular", ang_scale, 0.5);
+    nh_priv.param<double>("scale_linear", lin_scale, 0.5);
+    
     // Default constructed
-    TeleopP3AT teleop_p3at(nh, nh_priv);
+    TeleopP3AT teleop_p3at(nh, linear, angular, ang_scale, lin_scale);
 
     // Spin and leave work for callbacks
     ros::spin();
